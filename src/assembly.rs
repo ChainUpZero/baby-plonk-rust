2b9334a405fd879dbf7017a72c4551439ec667d7
isaacsue <805890851@qq.com>
2024-04-22 21:21:03 +0800

    function evaluate

--- src/assembly.rs
+++ src/assembly.rs
@@ -1,6 +1,6 @@
 use crate::utils::{extract_number_and_variable, split_expression};
 use bls12_381::Scalar;
-use ff::Field;
+use ff::{Field, PrimeField};
 #[derive(Debug, Clone)]
 pub struct GateWire {
     pub L: Option<String>,
@@ -145,5 +145,136 @@ mod tests {
             "'a <== c + d' to assembly_eqn:{:?}",
             AssemblyEqn::eq_to_assembly("a <== c + d")
         );
+    }
+}
+
+use std::{collections::HashMap, panic};
+
+fn evaluate(exprs: &Vec<&str>) -> HashMap<Option<String>, i64> {
+    evaluate_inner(exprs, false)
+}
+
+fn evaluate_inner(exprs: &Vec<&str>, first_is_negative: bool) -> HashMap<Option<String>, i64> {
+    match exprs.iter().any(|&x| x == "+") {
+        true => {
+            let idx = exprs.iter().position(|&x| x == "+").unwrap();
+            let l = evaluate_inner(&exprs[..idx].to_vec(), first_is_negative);
+            let r = evaluate_inner(&exprs[idx + 1..].to_vec(), false);
+            return merge_maps(&l, &r);
+        }
+        false => match exprs.iter().any(|&x| x == "-") {
+            true => {
+                let idx = exprs.iter().position(|&x| x == "-").unwrap();
+                let l = evaluate_inner(&exprs[..idx].to_vec(), first_is_negative);
+                let r = evaluate_inner(&exprs[idx + 1..].to_vec(), true);
+                return merge_maps(&l, &r);
+            }
+            false => match exprs.iter().any(|&x| x == "*") {
+                true => {
+                    let idx = exprs.iter().position(|&x| x == "*").unwrap();
+                    let l = evaluate_inner(&exprs[..idx].to_vec(), first_is_negative);
+                    let r = evaluate_inner(&exprs[idx + 1..].to_vec(), first_is_negative);
+                    return multiply_maps(&l, &r);
+                }
+                false => {
+                    if exprs.len() > 1 {
+                        panic!("No ops, expected sub-expr to be a unit: {:?}", exprs[1]);
+                    } else if exprs[0].starts_with('-') {
+                        return evaluate_inner(&vec![&exprs[0][1..]], !first_is_negative);
+                    } else if exprs[0].parse::<i64>().is_ok() {
+                        let value = exprs[0].parse::<i64>().unwrap()
+                            * if first_is_negative { -1 } else { 1 };
+                        let mut result = HashMap::new();
+                        result.insert(None, value);
+                        return result;
+                    } else if is_valid_variable_name(exprs[0]) {
+                        let mut result = HashMap::new();
+                        let value = if first_is_negative { -1 } else { 1i64 };
+                        result.insert(Some(exprs[0].to_string()), value);
+                        return result;
+                    } else {
+                        panic!("ok wtf is {}", exprs[0]);
+                    }
+                }
+            },
+        },
+    }
+}
+
+fn merge_maps(
+    map1: &HashMap<Option<String>, i64>,
+    map2: &HashMap<Option<String>, i64>,
+) -> HashMap<Option<String>, i64> {
+    let mut merged = HashMap::new();
+    for (key, val) in map1.iter().chain(map2.iter()) {
+        *merged.entry(key.clone()).or_insert(0) += val;
+    }
+    merged
+}
+
+fn multiply_maps(
+    map1: &HashMap<Option<String>, i64>,
+    map2: &HashMap<Option<String>, i64>,
+) -> HashMap<Option<String>, i64> {
+    let mut result = HashMap::new();
+    for (k1, v1) in map1.iter() {
+        for (k2, v2) in map2.iter() {
+            let product_key = get_product_key(k1.clone(), k2.clone());
+            *result.entry(product_key).or_insert(0) += v1 * v2;
+        }
+    }
+    result
+}
+
+fn get_product_key(key1: Option<String>, key2: Option<String>) -> Option<String> {
+    match (key1, key2) {
+        (Some(k1), Some(k2)) => {
+            let members = {
+                let mut members = Vec::new();
+                members.extend(k1.split('*'));
+                members.extend(k2.split('*'));
+                members.sort();
+                members
+            };
+            Some(
+                members
+                    .into_iter()
+                    .filter(|x| !x.is_empty())
+                    .collect::<Vec<&str>>()
+                    .join("*"),
+            )
+        }
+        (Some(k1), None) => Some(k1),
+        (None, Some(k2)) => Some(k2),
+        (None, None) => None,
+    }
+}
+
+fn is_valid_variable_name(name: &str) -> bool {
+    !name.is_empty()
+        && name.chars().all(char::is_alphanumeric)
+        && !name.chars().next().unwrap().is_numeric()
+}
+
+#[cfg(test)]
+mod test_eval {
+    use super::evaluate;
+    #[test]
+    fn test_evaluate() {
+        let expr = "6000 - 700 - 80 + 9";
+        let exprs = expr.split_whitespace().collect::<Vec<&str>>();
+        assert_eq!(*evaluate(&exprs).values().into_iter().next().unwrap(), 5229);
+        let expr = "-6000 + 700 + 80 - 9";
+        let exprs = expr.split_whitespace().collect::<Vec<&str>>();
+        assert_eq!(
+            *evaluate(&exprs).values().into_iter().next().unwrap(),
+            -5229
+        );
+        let expr = "1 + 2 * 3";
+        let exprs = expr.split_whitespace().collect::<Vec<&str>>();
+        assert_eq!(*evaluate(&exprs).values().into_iter().next().unwrap(), 7);
+        let expr = "-1 + 2 * 3";
+        let exprs = expr.split_whitespace().collect::<Vec<&str>>();
+        assert_eq!(*evaluate(&exprs).values().into_iter().next().unwrap(), 5);
     }
 }
